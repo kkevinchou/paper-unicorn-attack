2572a08f983fe538b656e52caa1eb4488e70171d
Yufei Guo <this.is.yufei@gmail.com>
2013-09-27 23:13:10 -0700

    clouds!

--- main.js
+++ main.js
@@ -1,3 +1,79 @@
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+
+
+(function() {
+    var resourceCache = {};
+    var loading = [];
+    var readyCallbacks = [];
+
+    // Load an image url or an array of image urls
+    function load(urlOrArr) {
+        if(urlOrArr instanceof Array) {
+            urlOrArr.forEach(function(url) {
+                _load(url);
+            });
+        }
+        else {
+            _load(urlOrArr);
+        }
+    }
+
+    function _load(url) {
+        if(resourceCache[url]) {
+            return resourceCache[url];
+        }
+        else {
+            var img = new Image();
+            img.onload = function() {
+                resourceCache[url] = img;
+
+                if(isReady()) {
+                    readyCallbacks.forEach(function(func) { func(); });
+                }
+            };
+            resourceCache[url] = false;
+            img.src = url;
+        }
+    }
+
+    function get(url) {
+        return resourceCache[url];
+    }
+
+    function isReady() {
+        var ready = true;
+        for(var k in resourceCache) {
+            if(resourceCache.hasOwnProperty(k) &&
+               !resourceCache[k]) {
+                ready = false;
+            }
+        }
+        return ready;
+    }
+
+    function onReady(func) {
+        readyCallbacks.push(func);
+    }
+
+    window.resources = { 
+        load: load,
+        get: get,
+        onReady: onReady,
+        isReady: isReady
+    };
+})();
+
+
+
 
 function drawBackground(context, canvas)
 {
@@ -10,21 +86,95 @@ function drawBackground(context, canvas)
     context.stroke();		
 }
 
+function Cloud(size) {
+	this.size = size;
+	this.image = new Image();
+	this.image.src = "cloud.png";
+	this.x = 800;
+	this.y = 20;
+}
+
+Cloud.prototype.draw = function(context)
+{
+	for (var i = 0; i < this.size; i++) {
+		var img = resources.get('cloud.png');
+		
+		context.drawImage(img, this.x + i*30, this.y, 50, 50);
+
+
+
+	};
+};
+
+var clouds = [];
+
+function generateClouds() {
+	var numClouds = 10;
+	for (var i = 0; i < numClouds; i++) {
+
+		clouds[i] = new Cloud(4);
+		clouds[i].y += 130*i;
+		clouds[i].x += 130*i;
+	};
+
+
+}
+
+var context;
+var canvas;
+function update(dt) {
+		for (var i = 0; i < clouds.length; i++) {
+			var cloud = clouds[i];
+			cloud.x -= 1;
+		};
+}
+
+function render() {
+		drawBackground(context, canvas);
+
+		for (var i = 0; i < clouds.length; i++) {
+			var cloud = clouds[i];
+			cloud.draw(context);
+		};
+}
+
+// The main game loop
+var lastTime;
+function main() {
+    var now = Date.now();
+    var dt = (now - lastTime) / 1000.0;
+
+    update(dt);
+    render();
+
+    lastTime = now;
+    requestAnimFrame(main);
+};
+
+function init() {
+   // reset();
+    lastTime = Date.now();
+    main();
+}
 
 $(document).ready(function(){
 
    // jQuery methods go here...
-   var canvas = document.getElementById('myDrawing');
+   canvas = document.getElementById('myDrawing');
 	// Check the element is in the DOM and the browser supports canvas
 	if(canvas.getContext) {
 		// Initaliase a 2-dimensional drawing context
-		var context = canvas.getContext('2d');
+		context = canvas.getContext('2d');
 		//Canvas commands go here
 
+
+		resources.load([
+		    'cloud.png'
+		]);
+
 		drawBackground(context, canvas);
-
-
-
+		generateClouds();
+		resources.onReady(init);
 	}
 
 });
