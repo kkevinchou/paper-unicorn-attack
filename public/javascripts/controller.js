951c7634441cf7b36a41538e6449f2d28f50df00
Aswinkumar Rajendiran <aswin.rajendiran@gmail.com>
2013-09-28 15:51:31 -0700

    Change x y for the swipe events.

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -6,6 +6,9 @@ var swipeStart = null;
 var inGame = false;
 var name = null;
 function ready () {
+	document.ontouchmove = function(event){
+		event.preventDefault();
+	}
 	setupSocketIO();
 	
 	showStartPage();
@@ -83,47 +86,49 @@ function setupController(data) {
 	$('#handle').text(data['name']);
 	$('#controller').css('background-color', data['color']);
 	$('#controller').swipe({
-    	swipeStatus:processSwipe
+		swipeStatus:processSwipe
     });
 }
 
 function processSwipe(event, phase, direction, distance, duration, fingers) {
 	var str = "<h4>Swipe Phase : " + phase + "<br/>";
+    str += "pagex: " + event.pageX + ", y: " + event.pageY  + "<br/>";
+    str += "clientx: " + event.clientX + ", y: " + event.clientY  + "<br/>";
     str += "x: " + event.x + ", y: " + event.y  + "<br/>";
 	if (phase == 'start') {
     	swipeStart = new Object();
-		swipeStart.x = event.x;
-		swipeStart.y = event.y;
+		swipeStart.x = event.pageX;
+		swipeStart.y = event.pageY;
 		var imgWidth = $("#pad").width();
 		var imgHeight = $("#pad").height();
 	} else if (phase == 'end') {
-		angle = calculateAngle(swipeStart.x, swipeStart.y, event.x, event.y);
-		magnitude = calculateMagnitude(swipeStart.x, swipeStart.y, event.x, event.y);
 		var eventType = 'move';
 		if (direction) {
+			angle = calculateAngle(swipeStart.x, swipeStart.y, event.pageX, event.pageY);
+			magnitude = calculateMagnitude(swipeStart.x, swipeStart.y, event.pageX, event.pageY);
 			eventType = 'move';
 			$("#pad").fadeOut(500);
 		} else {
 			eventType = 'tap';
 			centerX = $('#controller').width() / 2;
 			centerY = $('#controller').height() / 2;
-			angle = calculateAngle(centerX, centerY, event.x, event.y);
-			magnitude = calculateMagnitude(centerX, centerY, event.x, event.y);
+			angle = calculateAngle(centerX, centerY, swipeStart.x, swipeStart.y);
+			magnitude = calculateMagnitude(centerX, centerY, swipeStart.x, swipeStart.y);
 			$("#bomb").show();
 			var imgWidth = $("#bomb").width();
 			var imgHeight = $("#bomb").height();
-			$('#bomb').css({"position":"absolute", "top": (event.y - imgHeight/2) + "px", "left": (event.x - imgWidth/2) + "px"});
+			$('#bomb').css({"position":"absolute", "top": (swipeStart.y - imgHeight/2) + "px", "left": (swipeStart.x - imgWidth/2) + "px"});
 			$("#bomb").fadeOut(500);
 		}
 		socket.emit(eventType, {"name": name, "angle" :angle, "magnitude": magnitude});
 		swipeStart = null;
 	} else {
-		angle = calculateAngle(swipeStart.x, swipeStart.y, event.x, event.y);
+		angle = calculateAngle(swipeStart.x, swipeStart.y, event.pageX, event.pageY);
 		str += "Angle: " + angle + "<br/>";
 		var imgWidth = $("#pad").width();
 		var imgHeight = $("#pad").height();
 		$('#pad').show();
-		$('#pad').css({"position":"absolute", "top": (event.y - imgHeight/2) + "px", "left": (event.x - imgWidth/2) + "px"});
+		$('#pad').css({"position":"absolute", "top": (event.pageY - imgHeight/2) + "px", "left": (event.pageX - imgWidth/2) + "px"});
 	}
 	str += "Direction from inital touch: " + direction + "<br/>";
 	str += "Distance from inital touch: " + distance + "<br/>";
