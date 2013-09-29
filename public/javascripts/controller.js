66dd4c93f0b14c33968ceaf2ce5d9e220dc83a73
Michael Petrov <petrov.michael@gmail.com>
2013-09-29 11:49:27 -0700

    using cookie to store name in controller

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -80,6 +80,9 @@ function socketConnected() {
 function showStartPage() {
 	hideController();
 	$('#startPage').show();
+	if ($.cookie('unicorn-name')) {
+		$('input#name').val($.cookie('unicorn-name'));
+	}
 	inGame = false;
 	$('#join').unbind('click');
 	$('#join').click(function (data) {
@@ -97,6 +100,7 @@ function joinGame() {
 	name = $('#name').val();
 	if (name) {
 		socket.emit('join', {"name":name});
+		$.cookie('unicorn-name', name, { expires: 7 });
 		//showController();
 	} else {
 		alert('Enter a name');
